d5ffad274ffb35d53fae7e7545092f551e631cd4
Casey Meakin <casey.meakin@gmail.com>
2016-02-21 01:14:02 -0800

    removed uneeded dependencies from project.clj

--- project.clj
+++ project.clj
@@ -2,11 +2,7 @@
   :description "Code to parse an apache webserver log file"
   :url "https://github.com/CaseyAMeakin/apache-log-parser"
   :license "none"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-detector "0.0.2"]
-                 [net.sf.uadetector/uadetector-core "0.9.10"]
-                 [net.sf.uadetector/uadetector-resources "2013.10"]
-                 [org.slf4j/slf4j-simple "1.7.5"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :main apache-log-parser.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
